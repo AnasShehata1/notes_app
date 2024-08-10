fa1e0715760cc367c445369bb94178442c98bb90
anasmohamed <manas9109@gmail.com>
2024-08-10 17:24:06 +0300

    add dark theme and finish app bar

---
+++ lib/widgets/notes_views_body.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_app_bar.dart';
+
+class NotesViewsBody extends StatelessWidget {
+  const NotesViewsBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: [
+          SizedBox(height: 40),
+          CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
