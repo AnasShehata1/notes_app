6fd484af653ed1bf8703408abacddee4763dc6b9
anasmohamed <manas9109@gmail.com>
2024-08-15 23:16:45 +0300

    finish bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constant.dart';
+import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,13 +10,18 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          CustomTextField(hintText: 'Title'),
-          SizedBox(height: 18),
-          CustomTextField(hintText: 'Content', maxLines: 5),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32),
+            CustomTextField(hintText: 'Title'),
+            SizedBox(height: 18),
+            CustomTextField(hintText: 'Content', maxLines: 5),
+            SizedBox(height: 32),
+            CustomButton(text: 'Add'),
+            SizedBox(height: 18),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constant.dart';
 
 class CustomButton extends StatelessWidget {
   const CustomButton({super.key, required this.text, this.onTap});
@@ -10,14 +11,16 @@ class CustomButton extends StatelessWidget {
       onTap: onTap,
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(5),
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.circular(8),
         ),
         width: double.infinity,
-        height: 40,
+        height: 45,
         child: Center(
           child: Text(
             text,
+            style: const TextStyle(
+                color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600),
           ),
         ),
       ),
