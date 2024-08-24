c039c55c245dacbe35b82f82a2ba4aee0ce4f71f
anasmohamed <manas9109@gmail.com>
2024-08-25 00:36:59 +0300

    extract add note form widget

---
+++ lib/widgets/add_note_form.dart
@@ -0,0 +1,57 @@
+
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_button.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, content;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(height: 32),
+          CustomTextField(
+            hintText: 'Title',
+            onSaved: (data) {
+              title = data;
+            },
+          ),
+          const SizedBox(height: 18),
+          CustomTextField(
+            hintText: 'Content',
+            maxLines: 5,
+            onSaved: (data) {
+              content = data;
+            },
+          ),
+          const SizedBox(height: 32),
+          CustomButton(
+            text: 'Add',
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(height: 18),
+        ],
+      ),
+    );
+  }
+}
