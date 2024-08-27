5dc93cc17da7686de8af31c8020bdccba62a4b0e
anasmohamed <manas9109@gmail.com>
2024-08-27 18:36:55 +0300

    change add button in add note bottom sheet

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_state.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
@@ -39,20 +40,25 @@ class _AddNoteFormState extends State<AddNoteForm> {
             },
           ),
           const SizedBox(height: 32),
-          CustomButton(
-            text: 'Add',
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-                autovalidateMode = AutovalidateMode.always;
-                var noteModel = NoteModel(
-                    title: title!,
-                    subTitle: content!,
-                    dateTime: DateTime.now().toString(),
-                    color: Colors.blue.value);
-                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
-                setState(() {});
-              }
+          BlocBuilder<AddNoteCubit, AddNoteStates>(
+            builder: (context, state) {
+              return CustomButton(
+                isLoading: state is AddNoteLoading ? true : false,
+                text: 'Add',
+                onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    autovalidateMode = AutovalidateMode.always;
+                    var noteModel = NoteModel(
+                        title: title!,
+                        subTitle: content!,
+                        dateTime: DateTime.now().toString(),
+                        color: Colors.blue.value);
+                    BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
+                    setState(() {});
+                  }
+                },
+              );
             },
           ),
           const SizedBox(height: 18),
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constant.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.text, this.onTap});
+  const CustomButton(
+      {super.key, required this.text, this.onTap, this.isLoading = false});
   final String text;
   final VoidCallback? onTap;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -17,11 +19,21 @@ class CustomButton extends StatelessWidget {
         width: double.infinity,
         height: 45,
         child: Center(
-          child: Text(
-            text,
-            style: const TextStyle(
-                color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600),
-          ),
+          child: isLoading
+              ? const SizedBox(
+                  height: 25,
+                  width: 25,
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+                )
+              : Text(
+                  text,
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600),
+                ),
         ),
       ),
     );
